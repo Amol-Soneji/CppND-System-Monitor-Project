0bf22fa468f10bd12b48025dac6ab37c681b5f63
Amol <eon747@live.com>
2021-07-16 19:21:26 -0400

    Fixed issue I spotted in process.h

--- include/process.h
+++ include/process.h
@@ -19,8 +19,8 @@ class Process {
   // TODO: Declare any necessary private members
  private:
     int PID {0}; //Set PID to default of 0.  
-    string user {""}; //Set user to default of a blank string.  
-    string command {""}; //Set command to default of a blank string.
+    std::string user {""}; //Set user to default of a blank string.  
+    std::string command {""}; //Set command to default of a blank string.
     //All other values will mostly change repeatedly over time, thus it would be better that their respective functions call LinuxParser to parse their status each time.  
 };
 
